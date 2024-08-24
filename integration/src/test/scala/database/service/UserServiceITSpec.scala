5cd5bacc3af587844ad514d0e46c4404250e1f0c
David Pratt <dpratt747@gmail.com>
2024-08-24 22:27:37 +0100

    Add integration test for user deletion

--- integration/src/test/scala/database/service/UserServiceITSpec.scala
+++ integration/src/test/scala/database/service/UserServiceITSpec.scala
@@ -8,7 +8,7 @@ import domain.{PortDetails, User}
 import org.testcontainers.containers
 import util.{FlywayResource, TestContainerResource}
 import zio.jdbc.ZConnectionPoolConfig
-import zio.test.{TestAspect, ZIOSpecDefault, assertTrue}
+import zio.test.{Spec, TestAspect, TestEnvironment, ZIOSpecDefault, assertTrue}
 import zio.{Scope, ZIO, ZLayer}
 
 object UserServiceITSpec extends ZIOSpecDefault {
@@ -22,72 +22,104 @@ object UserServiceITSpec extends ZIOSpecDefault {
     postgresContainer.getPassword
   )
 
-  override def spec =
-    suite("UserService")(
-      test("can successfully insert the user") {
-        TestContainerResource.postgresResource.flatMap { postgresContainer =>
-          (for {
-            flyway <- FlywayResource.flywayResource(postgresContainer.getJdbcUrl, postgresContainer.getUsername, postgresContainer.getPassword)
-            validationResult <- ZIO.attempt(flyway.validateWithResult())
-            user <- ZIO.succeed(User("limbmissing", "David", "Pratt"))
-            _ <- ZIO.serviceWithZIO[UserServiceAlg](_.insertUser(user))
-          } yield assertTrue(
-            validationResult.validationSuccessful
-          )).provide(
-            connectionPoolConfigLayer(postgresContainer),
-            ZLayer.succeed(ZConnectionPoolConfig.default),
-            Scope.default,
-            UserRepository.live,
-            UserService.live
-          )
-        }
-      },
-      test("can successfully retrieve inserted users") {
-        TestContainerResource.postgresResource.flatMap { postgresContainer =>
-          (for {
-            flyway <- FlywayResource.flywayResource(postgresContainer.getJdbcUrl, postgresContainer.getUsername, postgresContainer.getPassword)
-            validationResult <- ZIO.attempt(flyway.validateWithResult())
-            user1 = User("limbmissing1", "David", "Pratt")
-            user2 = User("limbmissing2", "David", "Pratt")
-            user3 = User("limbmissing3", "David", "Pratt")
-            (insertUser, getAllUsers) <- ZIO.serviceWith[UserServiceAlg](service => (service.insertUser, service.getAllUsers))
-            _ <- insertUser(user1)
-            _ <- insertUser(user2)
-            _ <- insertUser(user3)
-            res <- getAllUsers
-          } yield assertTrue(
-            validationResult.validationSuccessful,
-            res.length == 3
-          )).provide(
-            connectionPoolConfigLayer(postgresContainer),
-            ZLayer.succeed(ZConnectionPoolConfig.default),
-            Scope.default,
-            UserRepository.live,
-            UserService.live
-          )
-        }
-      },
-      test("cannot insert the same username more than once") {
-        TestContainerResource.postgresResource.flatMap { postgresContainer =>
-          (for {
-            flyway <- FlywayResource.flywayResource(postgresContainer.getJdbcUrl, postgresContainer.getUsername, postgresContainer.getPassword)
-            validationResult <- ZIO.attempt(flyway.validateWithResult())
-            user = User("limbmissing", "David", "Pratt")
-            underTest <- ZIO.service[UserServiceAlg]
-            _ <- underTest.insertUser(user)
-            error <- underTest.insertUser(user).flip
-          } yield assertTrue(
-            validationResult.validationSuccessful,
-            error.isInstanceOf[UsernameDuplicateError]
-          )).provide(
-            connectionPoolConfigLayer(postgresContainer),
-            ZLayer.succeed(ZConnectionPoolConfig.default),
-            Scope.default,
-            UserRepository.live,
-            UserService.live
-          )
-        }
-      }
-    ) @@ TestAspect.timeout(zio.Duration.fromSeconds(35))
+  override def spec = suite("UserService")(
+    insertUserTests,
+    getAllUsersTest,
+    deleteUserByUsernameTest
+  ) @@ TestAspect.timeout(zio.Duration.fromSeconds(35))
 
+  private val insertUserTests = suite("insertUser")(
+    test("can successfully insert the user") {
+      TestContainerResource.postgresResource.flatMap { postgresContainer =>
+        (for {
+          flyway <- FlywayResource.flywayResource(postgresContainer.getJdbcUrl, postgresContainer.getUsername, postgresContainer.getPassword)
+          validationResult <- ZIO.attempt(flyway.validateWithResult())
+          user = User("limbmissing", "David", "Pratt")
+          res <- ZIO.serviceWithZIO[UserServiceAlg](_.insertUser(user))
+        } yield assertTrue(
+          validationResult.validationSuccessful,
+          res == ()
+        )).provide(
+          connectionPoolConfigLayer(postgresContainer),
+          ZLayer.succeed(ZConnectionPoolConfig.default),
+          Scope.default,
+          UserRepository.live,
+          UserService.live
+        )
+      }
+    },
+    test("cannot insert the same username more than once") {
+      TestContainerResource.postgresResource.flatMap { postgresContainer =>
+        (for {
+          flyway <- FlywayResource.flywayResource(postgresContainer.getJdbcUrl, postgresContainer.getUsername, postgresContainer.getPassword)
+          validationResult <- ZIO.attempt(flyway.validateWithResult())
+          user = User("limbmissing", "David", "Pratt")
+          underTest <- ZIO.service[UserServiceAlg]
+          _ <- underTest.insertUser(user)
+          error <- underTest.insertUser(user).flip
+        } yield assertTrue(
+          validationResult.validationSuccessful,
+          error.isInstanceOf[UsernameDuplicateError]
+        )).provide(
+          connectionPoolConfigLayer(postgresContainer),
+          ZLayer.succeed(ZConnectionPoolConfig.default),
+          Scope.default,
+          UserRepository.live,
+          UserService.live
+        )
+      }
+    }
+  )
+
+  private val getAllUsersTest = suite("getAllUsers")(
+    test("can successfully retrieve inserted users") {
+      TestContainerResource.postgresResource.flatMap { postgresContainer =>
+        (for {
+          flyway <- FlywayResource.flywayResource(postgresContainer.getJdbcUrl, postgresContainer.getUsername, postgresContainer.getPassword)
+          validationResult <- ZIO.attempt(flyway.validateWithResult())
+          user1 = User("limbmissing1", "David", "Pratt")
+          user2 = User("limbmissing2", "David", "Pratt")
+          user3 = User("limbmissing3", "David", "Pratt")
+          (insertUser, getAllUsers) <- ZIO.serviceWith[UserServiceAlg](service => (service.insertUser, service.getAllUsers))
+          _ <- insertUser(user1)
+          _ <- insertUser(user2)
+          _ <- insertUser(user3)
+          res <- getAllUsers
+        } yield assertTrue(
+          validationResult.validationSuccessful,
+          res.length == 3
+        )).provide(
+          connectionPoolConfigLayer(postgresContainer),
+          ZLayer.succeed(ZConnectionPoolConfig.default),
+          Scope.default,
+          UserRepository.live,
+          UserService.live
+        )
+      }
+    }
+  )
+
+  private val deleteUserByUsernameTest = suite("deleteUserByUsername")(
+    test("can delete a user by username") {
+      TestContainerResource.postgresResource.flatMap { postgresContainer =>
+        (for {
+          flyway <- FlywayResource.flywayResource(postgresContainer.getJdbcUrl, postgresContainer.getUsername, postgresContainer.getPassword)
+          validationResult <- ZIO.attempt(flyway.validateWithResult())
+          user = User("limbmissing", "David", "Pratt")
+          (insertUser, deleteUserByUsername) <- ZIO.serviceWith[UserServiceAlg](service => (service.insertUser, service.deleteUserByUsername))
+          _ <- insertUser(user)
+          res <- deleteUserByUsername(user.userName)
+        } yield assertTrue(
+          validationResult.validationSuccessful,
+          res == ()
+        )).provide(
+          connectionPoolConfigLayer(postgresContainer),
+          ZLayer.succeed(ZConnectionPoolConfig.default),
+          Scope.default,
+          UserRepository.live,
+          UserService.live
+        )
+      }
+    }
+  )
 }
